633cd4e46cb962f1f7943d896b66d47e6b5ad0e2
Vladislav Kuznetsov <Vladislav.Kuznetsov@jetbrains.com>
2021-10-02 13:43:32 +0300

    Update versions of gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val kotlin_css_version: String by project
-
 plugins {
     application
     kotlin("jvm") version "1.5.31"
@@ -20,11 +15,13 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:$ktor_version")
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
-    implementation("io.ktor:ktor-html-builder:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-css:$kotlin_css_version")
+    implementation("io.ktor:ktor-server-core:1.6.3")
+    implementation("io.ktor:ktor-server-netty:1.6.3")
+    implementation("ch.qos.logback:logback-classic:1.2.6")
+    testImplementation("io.ktor:ktor-server-tests:1.6.3")
+    implementation("io.ktor:ktor-html-builder:1.6.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.6.0-M1")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-css:1.0.0-pre.251-kotlin-1.5.31")
+    implementation("com.github.docker-java:docker-java:3.2.12")
+
 }
