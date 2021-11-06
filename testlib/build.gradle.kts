86fc0d17068732d743559824aae9aacaa0dc3264
Vladislav Kuznetsov <Vladislav.Kuznetsov@jetbrains.com>
2021-11-06 19:12:36 +0300

    Fix useless imports in gradle

--- testlib/build.gradle.kts
+++ testlib/build.gradle.kts
@@ -12,12 +12,5 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:1.6.4")
-    implementation("io.ktor:ktor-server-netty:1.6.4")
-    implementation("ch.qos.logback:logback-classic:1.2.6")
-    testImplementation("io.ktor:ktor-server-tests:1.6.4")
     implementation("io.ktor:ktor-html-builder:1.6.4")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-css:1.0.0-pre.251-kotlin-1.5.31")
-    implementation("com.github.docker-java:docker-java:3.2.12")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.6.0-M1")
 }
