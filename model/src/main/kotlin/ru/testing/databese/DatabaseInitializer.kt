85d0359f47a2d30cae0b7f9614b0ff91e6cbd90d
Vladislav Kuznetsov <Vladislav.Kuznetsov@jetbrains.com>
2021-12-11 17:38:35 +0300

    Database initializer from properties

--- model/src/main/kotlin/ru/testing/databese/DatabaseInitializer.kt
+++ model/src/main/kotlin/ru/testing/databese/DatabaseInitializer.kt
@@ -5,15 +5,25 @@ import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import ru.testing.databese.definition.Users
+import java.io.IOException
+import java.util.*
 
 class DatabaseInitializer : AbstractDatabaseInitializer {
     override fun createSchema() {
-        // TODO: make it nicer, build from properties
+        val properties = Properties()
+        try {
+            properties.load(DatabaseInitializer::class.java.getResourceAsStream("/database.properties"))
+        } catch (e: IOException) {
+            // TODO make normal
+        } catch (e: NullPointerException) {
+            // TODO make normal
+        }
+
         Database.connect(
-            "jdbc:postgresql://localhost:5432/testsys",
-            driver = "org.postgresql.Driver",
-            user = "vlad",
-            password = "vlad"
+            url = properties.getProperty("url"),
+            driver = properties.getProperty("driver"),
+            user = properties.getProperty("user"),
+            password = properties.getProperty("password")
         )
         transaction {
             SchemaUtils.create(Users)
